fe8e4c8a64edc7288bd65c8b5d8db6741db76a6f
Miki <ghermaico135@gmail.com>
2023-05-10 14:04:43 +0300

    Populating todo list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 /** @format */
 
-import _ from "lodash";
-import "./style.css";
+// import _ from 'lodash';
+import './style.css';
+
+const todoListContainer = document.querySelector('#todo-list-container');
+class Todo {
+  constructor() {
+    this.userTodo = [
+      {
+        description: 'Double-tab to edit',
+        completed: false,
+      },
+      {
+        description: "Drag'n drop to reorder your list",
+        completed: false,
+      },
+      {
+        description: 'Manage all your lists in one place',
+        completed: false,
+      },
+      {
+        description: 'Resync to clear out the old',
+        completed: false,
+      },
+    ];
+  }
+
+  showList() {
+    let content = '';
+
+    this.userTodo.forEach((todo) => {
+      content += `
+      <div class="todo-list">
+      <input type="checkbox" id="checkBtn" check=${todo.completed}/>
+      <p>${todo.description}</p>
+    </div>
+  
+      `;
+    });
+    todoListContainer.innerHTML = content;
+  }
+}
+
+const userTodo = new Todo();
+
+window.addEventListener('load', () => {
+  userTodo.showList();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,41 @@
 /** @format */
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.js",
-	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	devtool: "inline-source-map",
-	devServer: {
-		static: "./dist",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Output Management",
-		}),
-	],
-	optimization: {
-		runtimeChunk: "single",
-	},
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'todo list',
+      filename: './index.html',
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };

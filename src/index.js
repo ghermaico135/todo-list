777fee20893424f263c4c7a421b358edbe34a5a5
Miki <ghermaico135@gmail.com>
2023-05-10 16:41:07 +0300

    Adding index to object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /** @format */
 
+/** @format */
+
 // import _ from 'lodash';
 import './style.css';
 
@@ -7,19 +9,19 @@ const todoListContainer = document.querySelector('#todo-list-container');
 class Todo {
   constructor() {
     this.userTodo = [
+      { idex: 0, description: 'Double-tab to edit', completed: false },
       {
-        description: 'Double-tab to edit',
-        completed: false,
-      },
-      {
+        idex: 1,
         description: "Drag'n drop to reorder your list",
         completed: false,
       },
       {
+        index: 2,
         description: 'Manage all your lists in one place',
         completed: false,
       },
       {
+        idex: 3,
         description: 'Resync to clear out the old',
         completed: false,
       },
